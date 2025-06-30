8939926f91219de07556a94239f35750086c4360
Sukanya <88961511+Sukanya-24@users.noreply.github.com>
2025-07-01 01:55:45 +0530

    Add files via upload

--- SQL Scripts/createlogin.sql
+++ SQL Scripts/createlogin.sql
@@ -1,1 +1,5 @@
+--create login luke with password='123456789'
+--create user luke for login luke 
 
+create login sukanya2 with password='12345'
+create user sukanya2 for login sukanya2
---
+++ SQL Scripts/grantPriveleges.sql
@@ -0,0 +1,6 @@
+--use AdventureWorksLT2022;
+--grant select on SalesLT.Address to luke;
+--GRANT SELECT ON SCHEMA::SalesLT TO luke;
+
+use AdventureWorksLT2022;
+GRANT SELECT ON SCHEMA::SalesLT TO sukanya;
